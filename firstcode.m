2d876873064f08c1e76f3350ad438ff4ed617319
zcon18 <blackholezack@gmail.com>
2023-03-21 19:40:41 -0400

    Added comments, checks for the memory file, unstuck bouncing, and reverse function
    I made it so that if the robot is ever backed into a corner it will just go back the way it came from to avoid crashing, though it will continue that way and might backtrack a lot, its advisable we figure out away to pick a non-diagonal direction, to continue in.
    
    I made the program check to see if memorySpace.mat exists before calling on it to grab pD from.
    
    I added a function called reverse it takes in direction value and gives the direction value corresponding to the opposite of the keypad.
    
    The robot will get stuck if it approaches a lone black diagonally because the corner of its FOV is invisible, so we need a way of checking inside the 3x3 "safety square" if our approach is still going to be diagonally.
    
    I think I might call this short term straight line bot instead, cause its not really making a beeline towards anything.

--- firstcode.m
+++ firstcode.m
@@ -1,8 +1,26 @@
+%Zack Weinstein, Harrison Cotton, Emily Keller
 
-%local_view=zeros(5,5);
-%disp("Zack Weinstein, Harrison Cotton, Emily Keller")
-load('memorySpace.mat');
-previousDirectionIndex=((previousDirection-1)/2)+(previousDirection<5); %converts the digonal directon to the index
+%TODO: If the memorySpace ever starts a trial with a 5 in pD the robot will
+%never move, so wee need to fix that
+%TODO: The robot will get stuck if it approaches a lone black diagonally
+%because the corner of its FOV is invisible, so we need a way of checking
+%inside the 3x3 "safety square" if our approach is still using diagonals.
+%TODO: Pick a non-digonal direction when apporching a corner that the robot
+%gets stuck in
+
+
+%Mem needs to be called in both branches
+if isfile('memorySpace.mat') %There's a chance the file won't exist so we need to make it just in case
+    mem = matfile('memorySpace.mat','Writable',true); % Matfile makes it so we don't have to load a file in each time
+else
+    disp("memory space file does not exist, creating")
+    previousDirection=1; %Prefer to go to the bottom left
+    save("memorySpace.mat",'previousDirection');
+    mem=matfile('memorySpace.mat','Writable',true);
+end
+previousDirection=mem.previousDirection; %grabs pD from memory
+previousDirectionIndex=((previousDirection-1)/2)+(previousDirection<5); %converts the digonal directon to the index, it sets it to 2 on the first move but because the frame is zero this doesn't matter
+%Picking the best moves
 optimal_moves=[1,3,7,9];
 top=local_view(1,[2:4]);
 bottom=local_view(5,[2:4]);
@@ -24,7 +42,7 @@ if sum(right)<0
     optimal_moves(2)=0;
     optimal_moves(4)=0;
 end
-if step_num>0 %anti-backtracking
+if (step_num>0)&&(previousDirection~=5) %anti-backtracking, only starts after the first frame, and it had to move somewhere on the last frame
     %{
 3    4           0.5    1.5           -0.5 -1.5           2   1
   X       - 2.5               *-1                 + 2.5
@@ -32,40 +50,49 @@ if step_num>0 %anti-backtracking
     %}
     optimal_moves((previousDirectionIndex - 2.5).*(-1) + 2.5)=0; %this makes it so we can't backtrack
 end
+
+%Picking from the best moves
 possible_moves=[];
 for i=1:length(optimal_moves)
     if optimal_moves(i)>0
         possible_moves=[possible_moves,optimal_moves(i)]; %this commbines all non-zero vaules into a smaller array
     end
 end
-if(optimal_moves(previousDirectionIndex)==0)
-    disp(possible_moves);
+%Setting command to be the one we picked
+if(optimal_moves(previousDirectionIndex)==0) %Incase we can't go in a straight line we pick a new direction
+    if isempty(possible_moves) %this makes it so the robot can only back track if we are back into a corner
+        possible_moves=[reverse(previousDirection)];
+    end
     n=randi(length(possible_moves),1);
     previousDirection=possible_moves(n);
     
     command = possible_moves(n);
-    save('memorySpace.mat','previousDirection');
-else
+    mem.previousDirection=previousDirection;
+else %Otherwise we just go in a straight line
     command = previousDirection;
 end
 
 
-%test to see if I can get git to work
-
-%second test to see if its easier this time
-
-%{
-    for i=1:length(optimal_moves)
-        
-        if optimal_moves(i)>0
-            command=optimal_moves(i);
-            break
-        end
-    end 
-%}
-% TOP: local_view([2:4],1), BOTTOM local_view([2:4],5)
-% Left local_view(1,[2:4]) Right local_view(5,[2:4])
-%{
-    %pick rand diagonal
-    command = optimal_moves(randi(4,1))
-%}
+%functions
+function reversed=reverse(direction) %takes in a direction number gives the reverse direction, were the dirrection numbers are placed as if they are on e key-pad
+    switch direction
+        case 1
+            reversed = 9;
+        case 2
+            reversed = 8;
+        case 3
+            reversed = 7;
+        case 4
+            reversed = 6;
+        case 5
+            reversed = 5;
+        case 6
+            reversed = 4;
+        case 7
+            reversed = 3;
+        case 8
+            reversed = 2;
+        case 9
+            reversed = 1;
+    end
+end
